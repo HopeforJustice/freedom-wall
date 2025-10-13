27383d8cf4314e7c2a8a8a28d9be0fef80e38c77
James Holt <jamesphilipdavidholt@gmail.com>
2025-10-13 12:36:23 +0100

    fixing build errors

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,38 @@
 import { resolve } from "path";
 
-export default {
-	root: "src/",
-	publicDir: "../static/",
-	base: "./",
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "src/index.html"),
-				admin: resolve(__dirname, "src/admin/index.html"),
+export default ({ mode }) => {
+	const isProduction = mode === "production";
+
+	return {
+		root: "src/",
+		publicDir: "../static/",
+		base: "./",
+		build: {
+			rollupOptions: {
+				input: isProduction
+					? { main: resolve(__dirname, "src/index.html") }
+					: {
+							main: resolve(__dirname, "src/index.html"),
+							admin: resolve(__dirname, "src/admin/index.html"),
+					  },
+			},
+			outDir: "../dist",
+			emptyOutDir: true,
+			sourcemap: true,
+		},
+		server: {
+			host: true,
+			open: !(
+				"SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env
+			),
+			proxy: {
+				// Proxy API requests to the backend server
+				"/api": {
+					target: "http://localhost:3001",
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
-		outDir: "../dist",
-		emptyOutDir: true,
-		sourcemap: true,
-	},
-	server: {
-		host: true,
-		open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env),
-		proxy: {
-			// Proxy API requests to the backend server
-			"/api": {
-				target: "http://localhost:3001",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+	};
 };
