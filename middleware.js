d78f6d1a28218de8b4b05008bd5773b1dfef06c7
James Holt <jamesphilipdavidholt@gmail.com>
2025-10-17 16:16:51 +0100

    editing middleware

--- middleware.js
+++ middleware.js
@@ -1,12 +1,16 @@
 export default function middleware(request) {
 	const ip =
 		request.ip || request.headers.get("x-forwarded-for")?.split(",")[0];
-
+	console.log("Detected IP:", ip);
 	const headerCountry = request.headers.get("x-vercel-ip-country") || "Default";
 	const headerCity = request.headers.get("x-vercel-ip-city") || "Default City";
+	console.log("Detected Country from header:", headerCountry);
+	console.log("Detected City from header:", headerCity);
 
-	const existingCountry = request.cookies.get("user-country")?.value;
-	const existingCity = request.cookies.get("user-city")?.value;
+	const existingCountry =
+		request.cookies.get("user-country")?.value || "Default";
+	const existingCity =
+		request.cookies.get("user-city")?.value || "Default City";
 
 	const country = existingCountry || headerCountry;
 	const city = existingCity || headerCity;
